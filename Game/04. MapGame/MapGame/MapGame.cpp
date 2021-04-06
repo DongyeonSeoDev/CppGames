87a11a818f0dc3275a62c135037545de1fcbdad6
DongyeonSeo <seodye04@gmail.com>
2021-04-06 16:09:14 +0900

    기능추가: 맵 게임 추가

---
+++ Game/04. MapGame/MapGame/MapGame.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
+int currentX, currentY;
+
+char ShowMap[20][21] = {
+	"++++++++++++++++++++",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"+..................+",
+	"++++++++++++++++++++"
+};
+
+void gotoXY(int x, int y)
+{
+	HANDLE hOut;
+	COORD Cur;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	Cur.X = x;
+	Cur.Y = y;
+	SetConsoleCursorPosition(hOut, Cur);
+}
+
+void displayScreen()
+{
+	for (int y = 0; y < 20; y++)
+	{
+		for (int x = 0; x < 20; x++)
+		{
+			gotoXY(x, y);
+			_putch(ShowMap[y][x]);
+		}
+	}
+
+	gotoXY(currentX, currentY);
+	_putch('0');
+	gotoXY(30, 2);
+	cout << "Q : 종료";
+}
+
+void move(int dir)
+{
+	int dir_x = 0, dir_y = 0;
+
+	switch (dir)
+	{
+	case LEFT:
+		dir_x = -1;
+		break;
+	case RIGHT:
+		dir_x = 1;
+		break;
+	case UP:
+		dir_y = -1;
+		break;
+	case DOWN:
+		dir_y = 1;
+		break;
+	}
+
+	if (ShowMap[currentY + dir_y][currentX + dir_x] != '+')
+	{
+		currentX += dir_x;
+		currentY += dir_y;
+	}
+}
+
+int main()
+{
+	int ch;
+	currentX = 1;
+	currentY = 1;
+
+	while (true)
+	{
+		displayScreen();
+		ch = _getch();
+
+		if (ch == 0xE0 || ch == 0)
+		{
+			ch = _getch();
+			switch (ch)
+			{
+			case LEFT:
+			case RIGHT:
+			case UP:
+			case DOWN:
+				move(ch);
+				break;
+			}
+		}
+		else
+		{
+			ch = tolower(ch);
+			if (ch == 'q')
+			{
+				exit(0);
+			}
+			else if (ch == 'z')
+			{
+				ShowMap[currentY][currentX] = 'X';
+			}
+		}
+	}
+
+	return 0;
+}
