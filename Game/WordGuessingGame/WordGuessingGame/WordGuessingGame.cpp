9869c30f0ed1e74718f3fa176d7c71f629382c31
DongyeonSeo <seodye04@gmail.com>
2021-04-06 15:59:48 +0900

    기능추가: 단어 맞히기 게임 추가

---
+++ Game/WordGuessingGame/WordGuessingGame/WordGuessingGame.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <string.h>
+using namespace std;
+
+struct tag_Question {
+	string question;
+	string answer;
+	int wordCount;
+};
+
+tag_Question question[] = {
+	{"*****", "apple", 5}
+};
+
+int main()
+{
+	cout << "---------------------------------" << endl;
+	cout << "|     단어 맞히기 게임          |" << endl;
+	cout << "---------------------------------" << endl;
+	cout << "|   설명 : 알파벳을 누르세요.   |" << endl;
+	cout << "---------------------------------" << endl;
+
+	string tempQuestion = question[0].question;
+	char ch;
+	cout << tempQuestion;
+
+	while (tempQuestion != question[0].answer)
+	{
+		ch = _getch();
+		cout << " " << ch << endl;
+
+		for (int j = 0; j < question[0].wordCount; j++)
+		{
+			if (ch == question[0].answer[j])
+			{
+				tempQuestion[j] = ch;
+			}
+		}
+
+		cout << tempQuestion;
+	}
+
+	return 0;
+}
