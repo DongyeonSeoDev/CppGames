72f987edf903700403a2a9768936d116e7dba6be
sdy362 <seodye04@gmail.com>
2021-05-26 00:45:13 +0900

    기능추가: 슈팅게임 추가

---
+++ Game/11. ShootingGame/ShootingGame/Missile.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Missile
+{
+private:
+	int x;
+	int y;
+
+	bool bMoveLeft; // 왼쪽으로 이동 여부
+
+public:
+	void setMissile(int _y);
+	void moveMissile();
+	void drawMissile();
+
+	bool checkEnd();
+	bool checkCollision(Missile missile);
+};
---
+++ Game/11. ShootingGame/ShootingGame/ShootingGame.cpp
@@ -0,0 +1,131 @@
+﻿
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+#include "Console.h"
+#include "Missile.h"
+
+using namespace std;
+
+bool bMoveDown;
+int y;
+
+vector<Missile> missiles, myMissiles;
+
+void setConsoleView()
+{
+	system("mode con:cols=60 lines=20");
+	system("title SHOOTING GAME");
+}
+
+int main()
+{
+	bMoveDown = true;
+	y = 0;
+
+	missiles.clear();
+
+	srand((unsigned)time(NULL));
+
+	setConsoleView();
+
+	int key;
+
+	while (true)
+	{
+		key = getKeyDown();
+		key = tolower(key);
+
+		if (key == 'a')
+		{
+			Missile myMissile;
+			myMissile.setMissile(y);
+
+			myMissiles.push_back(myMissile);
+		}
+
+		if (rand() % 100 < 10)
+		{
+			Missile missile;
+			missile.setMissile(-1);
+
+			missiles.push_back(missile);
+		}
+
+		for (int i = 0; i < (int)missiles.size(); i++)
+		{
+			missiles[i].moveMissile();
+			missiles[i].drawMissile();
+		}
+
+		for (int i = 0; i < (int)myMissiles.size(); i++)
+		{
+			myMissiles[i].moveMissile();
+			myMissiles[i].drawMissile();
+		}
+
+		gotoXY(2, y);
+		cout << "▶";
+		gotoXY(59, 19);
+
+		if (bMoveDown)
+		{
+			y++;
+
+			if (y > 19)
+			{
+				y = 19;
+				bMoveDown = false;
+			}
+		}
+		else
+		{
+			y--;
+
+			if (y < 0)
+			{
+				y = 0;
+				bMoveDown = true;
+			}
+		}
+
+		for (int i = 0; i < (int)missiles.size(); i++)
+		{
+			if (missiles[i].checkEnd())
+			{
+				missiles.erase(missiles.begin() + i);
+				i--;
+			}
+		}
+
+		for (int i = 0; i < (int)myMissiles.size(); i++)
+		{
+			if (myMissiles[i].checkEnd())
+			{
+				myMissiles.erase(myMissiles.begin() + i);
+				i--;
+			}
+		}
+
+		for (int i = 0; i < (int)myMissiles.size(); i++)
+		{
+			for (int j = 0; j < (int)missiles.size(); j++)
+			{
+				if (myMissiles[i].checkCollision(missiles[j]))
+				{
+					myMissiles.erase(myMissiles.begin() + i);
+					i--;
+
+					missiles.erase(missiles.begin() + j);
+					break;
+				}
+			}
+		}
+
+		sleep(50);
+		clrscr();
+	}
+
+	return 0;
+}
