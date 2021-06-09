b6a88f43ac214c782b5e0898b2342813d3f21e78
sdy362 <seodye04@gmail.com>
2021-06-10 00:15:05 +0900

    기능추가: 시작 화면, 설명 화면, 게임오버 화면 추가

--- Game/09. GoingUpGame/GoingUpGame/GoingUpGame.cpp
+++ Game/09. GoingUpGame/GoingUpGame/GoingUpGame.cpp
@@ -8,12 +8,31 @@
 using namespace std;
 
 #define mapXSize 40
-#define mapYSize 22
+#define mapYSize 23
 
 #define UP		72
 #define DOWN	80
 #define LEFT	75
 #define RIGHT	77
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWV 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
+
+#define FNAME "score"
 
 int playerX, playerY;
 
@@ -23,53 +42,163 @@ vector<int> itemX, itemY, itemData;
 char map[mapYSize][mapXSize] =
 {
 	"---------------------------------------",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
 	"---------------------------------------"
 };
 
 char map2[mapYSize][mapXSize] =
 {
 	"---------------------------------------",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
-	"@                                     @",
-	"*                                     *",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"---------------------------------------"
+};
+
+char map3[mapYSize][mapXSize] =
+{
 	"---------------------------------------",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"●                                   ●",
+	"▼                                   ▼",
+	"★                                   ★",
+	"---------------------------------------"
+};
+
+char startMap[mapYSize][mapXSize + 1] =
+{
+	"■■■■■■■■■■■■■■■■■■■■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■              UP! 게임              ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■   설명을 보려면 Q키를 눌러주세요   ■",
+	"■                                    ■",
+	"■    시작하려면 S키를 눌러주세요     ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■■■■■■■■■■■■■■■■■■■■"
+};
+
+char descriptionMap[mapYSize][mapXSize + 1] =
+{
+	"■■■■■■■■■■■■■■■■■■■■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                설명                ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■    돌아가려면 X키를 눌러주세요     ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■■■■■■■■■■■■■■■■■■■■"
+};
+
+char gameOverMap[mapYSize][mapXSize + 1] =
+{
+	"■■■■■■■■■■■■■■■■■■■■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■              게임오버              ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■    종료하려면 Q키를 눌러주세요     ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■                                    ■",
+	"■■■■■■■■■■■■■■■■■■■■"
 };
 
 void gotoXY(int x, int y)
@@ -88,8 +217,74 @@ void setcursor(bool i, DWORD size)
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &c);
 }
 
+void scolor(unsigned short text = 15, unsigned short back = 0)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text | (back << 4));
+}
+
+void start()
+{
+	system("cls");
+
+	scolor(YELLOW);
+	cout << endl << endl << endl << endl;
+	for (int i = 0; i < mapYSize; i++)
+	{
+		cout << "                                   ";
+
+		for (int j = 0; j < mapXSize + 1; j++)
+		{
+			cout << startMap[i][j];
+		}
+
+		cout << endl;
+	}
+	scolor();
+}
+
+void description()
+{
+	system("cls");
+	
+	scolor(LIGHTGREEN);
+	cout << endl << endl << endl << endl;
+	for (int i = 0; i < mapYSize; i++)
+	{
+		cout << "                                   ";
+
+		for (int j = 0; j < mapXSize + 1; j++)
+		{
+			cout << descriptionMap[i][j];
+		}
+
+		cout << endl;
+	}
+	scolor();
+}
+
+void gameOver()
+{
+	system("cls");
+
+	scolor(LIGHTRED);
+	cout << endl << endl << endl << endl;
+	for (int i = 0; i < mapYSize; i++)
+	{
+		cout << "                                   ";
+
+		for (int j = 0; j < mapXSize + 1; j++)
+		{
+			cout << gameOverMap[i][j];
+		}
+
+		cout << endl;
+	}
+	scolor();
+}
+
 void drawMap(char map[mapYSize][mapXSize])
 {
+	scolor(YELLOW);
 	for (int i = 0; i < mapYSize; i++)
 	{
 		cout << "                                   ";
@@ -102,10 +297,9 @@ void drawMap(char map[mapYSize][mapXSize])
 		cout << endl;
 	}
 
-	cout << endl << endl << endl;
-
+	scolor(LIGHTCYAN);
 	gotoXY(playerX, playerY);
-	cout << "&";
+	cout << "▲";
 
 	for (int i = 0; i < (int)itemData.size(); i++)
 	{
@@ -113,19 +307,24 @@ void drawMap(char map[mapYSize][mapXSize])
 
 		if (itemData[i] == 0)
 		{
-			cout << "0";
+			scolor(LIGHTBLUE);
+			cout << "♣";
 		}
 		else
 		{
-			cout << "1";
+			scolor(LIGHTGREEN);
+			cout << "♥";
 		}
 	}
 
 	for (int i = 0; i < (int)enemyX.size(); i++)
 	{
 		gotoXY(enemyX[i], enemyY[i]);
-		cout << "%";
+		scolor(LIGHTRED);
+		cout << "◆";
 	}
+
+	scolor();
 }
 
 int main()
@@ -144,7 +343,39 @@ int main()
 	int startItemHeight = 0;
 
 	playerX = 27;
-	playerY = 20;
+	playerY = 21;
+
+	start();
+
+	while (true)
+	{
+		char ch = _getch();
+		ch = tolower(ch);
+
+		if (ch == 's')
+		{
+			Beep(784, 200);
+			break;
+		}
+		else if (ch == 'q')
+		{
+			Beep(698, 200);
+			description();
+
+			while (true)
+			{
+				char ch = _getch();
+				ch = tolower(ch);
+
+				if (ch == 'x')
+				{
+					Beep(698, 200);
+					start();
+					break;
+				}
+			}
+		}
+	}
 
 	clock_t start = clock();
 	PlaySound(TEXT("Music.wav"), NULL, SND_ASYNC | SND_LOOP);
@@ -154,12 +385,12 @@ int main()
 		if (GetAsyncKeyState(VK_LEFT)) { //왼쪽
 			playerX--;
 
-			if (playerX < 18) playerX = 18;
+			if (playerX < 19) playerX = 19;
 		}
 		if (GetAsyncKeyState(VK_RIGHT)) { //오른쪽
 			playerX++;
 
-			if (playerX > 36) playerX = 36;
+			if (playerX > 35) playerX = 35;
 		}
 		if (GetAsyncKeyState(VK_UP)) { //위
 			playerY--;
@@ -169,44 +400,51 @@ int main()
 		if (GetAsyncKeyState(VK_DOWN)) { //아래
 			playerY++;
 
-			if (playerY > 24) playerY = 24;
+			if (playerY > 25) playerY = 25;
 		}
 
 		system("cls");
 
+		scolor(GREEN);
 		cout << "높이: " << height << "m" << endl;
+		scolor(YELLOW);
 		cout << "스피드: " << speed << endl;
-
+		scolor(LIGHTBLUE);
 		cout << "시간: " << difftime(clock(), start) / CLOCKS_PER_SEC << "초" << endl;
+		scolor();
 
 		if (isInvincibility) cout << "무적시간" << endl;
 		else cout << endl;
 
-		if (height % 2 == 0)
+		if (height % 3 == 0)
 		{
 			drawMap(map);
 		}
-		else
+		else if (height % 3 == 1)
 		{
 			drawMap(map2);
+		}
+		else
+		{
+			drawMap(map3);
 		}
 
 		if (rand() % 100 < creatEnemyProbability)
 		{
-			enemyX.push_back(rand() % 19 + 18);
+			enemyX.push_back(rand() % 17 + 19);
 			enemyY.push_back(4);
 		}
 
 		if (rand() % 100 < 1)
 		{
-			itemX.push_back(rand() % 19 + 18);
+			itemX.push_back(rand() % 17 + 19);
 			itemY.push_back(4);
 			itemData.push_back(0);
 		}
 
 		if (rand() % 100 < 1)
 		{
-			itemX.push_back(rand() % 19 + 18);
+			itemX.push_back(rand() % 17 + 19);
 			itemY.push_back(4);
 			itemData.push_back(1);
 		}
@@ -215,20 +453,36 @@ int main()
 		{
 			if (!isInvincibility && playerX == enemyX[i] && (playerY == enemyY[i] || playerY == enemyY[i] + 1))
 			{
-				system("cls");
+				gameOver();
+				gotoXY(23, 13);
+				scolor(GREEN);
 				cout << "높이: " << height << "m" << endl;
+				gotoXY(23, 15);
+				scolor(YELLOW);
 				cout << "스피드: " << speed << endl;
+				gotoXY(23, 17);
+				scolor(LIGHTBLUE);
 				cout << "시간: " << difftime(clock(), start) / CLOCKS_PER_SEC << "초" << endl;
-				gotoXY(10, 10);
-				cout << "게임오버" << endl << endl << endl << endl << endl << endl << endl;
+				gotoXY(0, 26);
+				scolor();
+				PlaySound(NULL, 0, 0);
 				Beep(988, 1000);
-				system("pause");
-				return 0;
+
+				while (true)
+				{
+					char ch = _getch();
+					ch = tolower(ch);
+
+					if (ch == 'q')
+					{
+						return 0;
+					}
+				}
 			}
 
 			enemyY[i]++;
 
-			if (enemyY[i] > 24)
+			if (enemyY[i] > 25)
 			{
 				enemyX.erase(enemyX.begin() + i);
 				enemyY.erase(enemyY.begin() + i);
@@ -259,7 +513,7 @@ int main()
 
 			itemY[i]++;
 
-			if (itemY[i] > 24)
+			if (itemY[i] > 25)
 			{
 				itemX.erase(itemX.begin() + i);
 				itemY.erase(itemY.begin() + i);
